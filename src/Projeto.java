ed2ea11ec3f25752a218da589720b4abacc80726
leonardobt14 <leonardo3ktt@gmail.com>
2020-05-29 19:50:19 -0300

    Tratativas 1 e 2

--- src/Projeto.java
+++ src/Projeto.java
@@ -9,9 +9,12 @@ public class Projeto {
 		Scanner sc = new Scanner(System.in);
 
 		String nome;
-		int horastrabalhadas, opcaomenu;
-		double valorhora;
+		int horastrabalhadas, opcaomenu, totalhoras;
+		double valorhora, custototal, custocada;
 		char outrofuncionario;
+		
+		totalhoras = 0;
+		custototal = 0;
 
 		do {System.out.print("Nome: ");
 		nome = sc.next();
@@ -36,14 +39,29 @@ public class Projeto {
 			System.out.print("Tentativa Inválida! Digite apenas S (sim) ou N (não)");
 			outrofuncionario = sc.next().charAt(0);
 		}
+		totalhoras = totalhoras + horastrabalhadas;
+		custocada = horastrabalhadas * valorhora;
+		custototal = custototal + custocada;
 		} while (outrofuncionario == 'S' || outrofuncionario == 's');
 		
 		
 		do {
 		System.out.println();	
 		System.out.println("MENU");
+		System.out.println("1 - Total de horas trabalhadas");
+		System.out.println("2 - Custo total");
 		System.out.print("Digite uma opção: ");
 		opcaomenu = sc.nextInt();
+		
+		if (opcaomenu == 1) {
+			System.out.println();
+			System.out.println("Total de horas = " + totalhoras);
+		}
+		if (opcaomenu == 2) {
+			System.out.println();
+			System.out.printf("Custo total = R$ %.2f%n" , custototal);
+		}
+		
 		} while (opcaomenu != 4);
 
 		sc.close();
